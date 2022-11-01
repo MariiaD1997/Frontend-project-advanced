eaa9c7448fb5ca0f176229075c72dec9804b9480
MariiaD1997 <mariadatskiv@gmail.com>
2022-11-01 21:54:44 +0200

    create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
+import Cart from "./Components/Cart";
+import Home from "./Components/Home";
+import ProductItem from "./Components/ProductItem";
+import Products from "./Components/Products";
+import Profile from "./Components/Profile";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <BrowserRouter>
+        <div>
+          <Link to="">Home</Link>
+          <Link to="products">Products</Link>
+          <Link to="cart">Cart</Link>
+          <Link to="profile">Profile</Link>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products">
+            <Route path="" element={<Products />} />
+            <Route path=":id" element={<ProductItem />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/ProductItem.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useParams } from "react-router";
+
+const ProductItem = () => {
+  const params = useParams();
+
+  return <div>ProductItem</div>;
+};
+
+export default ProductItem;
